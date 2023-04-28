651574b5d02a0f5f043d554306636b83ca2b7526
mjovetic16 <mjovetic16@raf.rs>
2023-04-28 14:48:11 +0200

    Added TreeView for whole file parsing

--- Java/FxForm/src/main/java/com/example/fxform/HelloController.java
+++ Java/FxForm/src/main/java/com/example/fxform/HelloController.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.apache.http.HttpEntity;
@@ -42,6 +44,25 @@ public class HelloController {
     private Label sendFormBtnLabel;
     @FXML
     private Label sendFileFormBtnLabel;
+
+    @FXML private TreeView<String> modelTreeView;
+
+    // the initialize method is automatically invoked by the FXMLLoader - it's magic
+    public void initialize() {
+        loadTreeItems("Empty Data");
+    }
+
+    // loads some strings into the tree in the application UI.
+    public void loadTreeItems(String... rootItems) {
+        TreeItem<String> root = new TreeItem<String>("Empty Root Node");
+        root.setExpanded(true);
+
+        for (String itemString: rootItems) {
+            root.getChildren().add(new TreeItem<String>(itemString));
+        }
+
+        modelTreeView.setRoot(root);
+    }
 
     @FXML
     protected void onSendButtonClick() {
@@ -138,7 +159,7 @@ public class HelloController {
 
             JsonMessage jsonMessage = gson.fromJson(content, JsonMessage.class);
             System.out.println(jsonMessage);
-            sendFormBtnLabel.setText("PARSED:"+jsonMessage.getMbody());
+            sendFormBtnLabel.setText("PARSED");
 
 
 
@@ -147,6 +168,9 @@ public class HelloController {
                Marc newMarc = parseJson(jsonMessage.getMbody());
                System.out.println("Parsed into marc object");
                System.out.println(newMarc);
+
+               loadMarcTree(newMarc);
+
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -380,4 +404,82 @@ public class HelloController {
 
         return marcLine;
     }
+
+    public void loadMarcTree(Marc marc){
+
+        TreeItem<String> root = new TreeItem<String>("MARC");
+
+        if(marc.getLeader()!=null){
+            TreeItem<String> leader = new TreeItem<String>("Leader");
+            TreeItem<String> leaderData = new TreeItem<>(marc.getLeader().getData());
+
+            leader.getChildren().add(leaderData);
+            root.getChildren().add(leader);
+        }
+
+        ArrayList<ControlField> controlFields = new ArrayList<>();
+        ArrayList<DataField> dataFields = new ArrayList<>();
+
+        if(marc.getControlFieldList().size()>0){
+            controlFields = (ArrayList<ControlField>) marc.getControlFieldList();
+        }
+
+        if(marc.getDataFieldList().size()>0){
+            dataFields = (ArrayList<DataField>) marc.getDataFieldList();
+        }
+
+
+        TreeItem<String> controlRoot = new TreeItem<>("Control Fields");
+        root.getChildren().add(controlRoot);
+
+        for(ControlField controlField:controlFields){
+            TreeItem<String> controlItem = new TreeItem<>();
+            TreeItem<String> controlItemData = new TreeItem<>();
+
+
+            controlItem.setValue(controlField.getTag());
+
+            controlItemData.setValue(controlField.getData());
+
+
+            controlItem.getChildren().add(controlItemData);
+
+            controlRoot.getChildren().add(controlItem);
+
+        }
+
+        TreeItem<String> dataRoot = new TreeItem<>("Data Fields");
+        root.getChildren().add(dataRoot);
+
+        for(DataField dataField:dataFields){
+
+            TreeItem<String> dataItem = new TreeItem<>(dataField.getTag());
+            TreeItem<String> dataItemIndex1 = new TreeItem<>("IND1: "+dataField.getIndex1());
+            TreeItem<String> dataItemIndex2 = new TreeItem<>("IND2: "+dataField.getIndex2());
+
+            TreeItem<String> dataItemSubRoot = new TreeItem<>("Subfields");
+
+            for(SubField subField:dataField.getSubFieldList()){
+
+                TreeItem<String> dataItemSubField = new TreeItem<>(subField.getTag());
+                TreeItem<String> dataItemSubFieldData = new TreeItem<>(subField.getData());
+
+                dataItemSubRoot.getChildren().add(dataItemSubField);
+                dataItemSubField.getChildren().add(dataItemSubFieldData);
+
+            }
+
+            dataRoot.getChildren().add(dataItem);
+            dataItem.getChildren().add(dataItemIndex1);
+            dataItem.getChildren().add(dataItemIndex2);
+            dataItem.getChildren().add(dataItemSubRoot);
+
+        }
+
+
+        modelTreeView.setRoot(root);
+
+    }
+
+
 }
